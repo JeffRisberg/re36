60c04bb4d44e8774935d7e6dc4becac98b279553
JeffRisberg <j.s.risberg@gmail.com>
2024-01-21 23:25:32 -0800

    updated to match chapter 8

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+function Transition() {
+      const [isRunning, setRunning] = useState(false);
+  const div = useRef();
+  useEffect(() => {
+    const onStart = () => setRunning(true);
+    const onEnd = () => setRunning(false);
+    const node = div.current;
+    node.addEventListener("transitionstart", onStart);
+    node.addEventListener("transitionend", onEnd);
+    return () => {
+      node.removeEventListener(
+          "transitionstart", onStart
+      );
+      node.removeEventListener("transitionend", onEnd);
+    };
+  }, [setRunning]);
 
-function Admin() {
-  const [password, setPassword] = useState("");
-  const [isAdmin, setAdmin] = useState(false);
-  const onClick = () => {
-    if (password === "platypus") {
-      setAdmin(true);
-    }
-  };
   return (
-    <>
-      {isAdmin && <h1>Bacon is delicious!</h1>}
-      <form>
-        <input
-          type="password"
-          onChange={(evt) => setPassword(evt.target.value)}
-        />
-        <button onClick={onClick}>
-          Login
+      <section>
+        <h1>Transition is {!isRunning && "not"} running</h1>
+        <div
+            style={{ color: "red", transition: "color 1s linear" }}
+            ref={div} >
+          COLORFUL TEXT
+        </div>
+        <button onClick={() => (div.current.style.color = "blue")}>
+          Go blue
         </button>
-      </form>
-    </>
+        <button onClick={() => (div.current.style.color = "red")}>
+          Go red
+        </button>
+      </section>
   );
 }
-
 function App() {
-  return <Admin />;
+  return <Transition />;
 }
-
 export default App;
